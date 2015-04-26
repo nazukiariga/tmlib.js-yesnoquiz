d6d48d3025abbe49aeb3e283c10a45064ff5f36a
phi <phi1618jp@gmail.com>
2015-04-27 02:43:21 +0900

    adjust script file

---
+++ scripts/constant.js
@@ -0,0 +1,39 @@
+/*
+ * constant.js
+ */
+
+// 読み込む画像, 音
+var ASSETS = {
+    // image
+    'correct': 'images/correct.png',
+    'incorrect': 'images/incorrect.png',
+    // se
+    'correct_se': 'sounds/correct.mp3',
+    'incorrect_se': 'sounds/incorrect.mp3',
+    // bgm
+    'bgm': 'sounds/bgm.m4a',
+};
+
+// 問題のリスト
+var QUESTIONS = [
+    {
+        text: '「ハッカー」とは「不正行為を行う人」\nのことである',
+        answer: false,
+    },
+    {
+        text: '1+1*-(1+10)=-10',
+        answer: true,
+    },
+    {
+        text: '上り坂より下り坂のほうが多い',
+        answer: false,
+    },
+    {
+        text: 'マクドナルドのロゴマークは、\nアルファベットの「Ｍ」である。',
+        answer: false,
+    },
+    {
+        text: '地上で働いているアリは、\nおばあさんアリである。',
+        answer: true,
+    },
+];
--- scripts/main.js
+++ scripts/main.js
@@ -3,48 +3,11 @@
  */
 
 // 初期化
-// 読み込む画像, 音
-var ASSETS = {
-    // image
-    'correct': 'images/correct.png',
-    'incorrect': 'images/incorrect.png',
-    // se
-    'correct_se': 'sounds/correct.mp3',
-    'incorrect_se': 'sounds/incorrect.mp3',
-    // bgm
-    'bgm': 'sounds/bgm.m4a',
-};
-
-// 問題のリスト
-var QUESTIONS = [
-    {
-        text: '「ハッカー」とは「不正行為を行う人」\nのことである',
-        answer: false,
-    },
-    {
-        text: '1+1*-(1+10)=-10',
-        answer: true,
-    },
-    {
-        text: '上り坂より下り坂のほうが多い',
-        answer: false,
-    },
-    {
-        text: 'マクドナルドのロゴマークは、\nアルファベットの「Ｍ」である。',
-        answer: false,
-    },
-    {
-        text: '地上で働いているアリは、\nおばあさんアリである。',
-        answer: true,
-    },
-];
-
-// 初期化
 tm.game.setup({
     title: "Yes/No クイーズ",
     width: 640,
     height: 960,
-    startLabel: "game",
+    startLabel: "title",
 });
 
 /*
@@ -154,7 +117,7 @@ tm.define("GameScene", {
             this.score--;
             // 不正解音を再生
             SoundManager.play('incorrect_se');
-            
+
             // 次の問題をセット
             this.setQuestion();
         }
